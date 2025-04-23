cc29e0e38f96109ffe92e76b226310718c33b8d8
gokulcodes <gokulvaradan2202@gmail.com>
2025-04-23 08:59:34 +0530

    Added virtualKeyboard on focus for mobile devices

--- src/components/EditorView.tsx
+++ src/components/EditorView.tsx
@@ -1,9 +1,13 @@
 import editorContext from "@/controller/editorContext";
-import { createElement, useEffect, useContext } from "react";
+import { createElement, useContext, useRef } from "react";
 export default function EditorView() {
   const { state, dispatch } = useContext(editorContext);
   const editor = state.editor
-  useEffect(() => {
+  const inputRef = useRef(null)
+  // useEffect(() => {
+  // }, [editor, dispatch]);
+  
+  function handleFocus() {
     let line = editor.insertLine(editor.linePtr);
     let cursor = line.letterPtr;
     function handleKeyDown(event: KeyboardEvent) {
@@ -13,71 +17,63 @@ export default function EditorView() {
         dispatch({ type: "type", payload: editor });
         return;
       }
-
+  
       if (event.key === "ArrowLeft") {
         cursor = line.moveLetterPtr(-1);
         dispatch({ type: "type", payload: editor });
         return;
       }
-
+  
       if (event.key === "ArrowRight") {
         cursor = line.moveLetterPtr(1);
         dispatch({ type: "type", payload: editor });
         return;
       }
-
+  
       if (event.key === "ArrowUp") {
         line = editor.moveLinePtr(-1);
         cursor = line.letterPtr;
         dispatch({ type: "type", payload: editor });
         return;
       }
-
+  
       if (event.key === "ArrowDown") {
         line = editor.moveLinePtr(1);
         cursor = line.letterPtr;
         dispatch({ type: "type", payload: editor });
         return;
       }
-
+  
       if (cursor == null) {
         cursor = line.insertLetter(cursor, event.key);
         dispatch({ type: "type", payload: editor });
         return;
       }
-
+  
       if (event.key === "Backspace") {
         cursor = line.deleteLettersFromStartToEnd(cursor, cursor);
         dispatch({ type: "type", payload: editor });
         return;
       }
-
+  
       if (event.key === "Shift") {
         dispatch({ type: "type", payload: editor });
         return;
       }
-
+  
       cursor = line.insertLetter(cursor, event.key);
       dispatch({ type: "type", payload: editor });
     }
     document.removeEventListener("keydown", handleKeyDown);
     document.addEventListener("keydown", handleKeyDown);
-  }, [editor, dispatch]);
-
-  function handleFocus() {
-    /* eslint-disable  @typescript-eslint/no-explicit-any */
-    const newNavigator: any = window.navigator;
-    if (newNavigator?.virtualKeyboard) {
-      newNavigator.virtualKeyboard.overlaysContent = true
-      newNavigator.virtualKeyboard.show()
-    }
   }
 
   return (
-    <div className="p-10" onClick={handleFocus} onTouchStart={handleFocus} >
+    <div className="p-10 relative">
       {editor.map(editor.line, (str: string) => {
         return createElement("p", { dangerouslySetInnerHTML: { __html: str } })
       })}
+      <input ref={inputRef} onClick={handleFocus} className="opacity-0 h-full w-full bg-white absolute top-0 left-0"  />
     </div>
   );
 }
